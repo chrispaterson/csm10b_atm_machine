1527ed011224538a6507d80b2409364de907c4a5
Chris Paterson <cpaterson@conversantmedia.com>
2017-12-10 19:48:36 -0800

    Clearing stuff out

--- src/ATMController.cpp
+++
@@ -1,9 +0,0 @@
-#include "ATMController.h"
-void ATMController:: clearConsole() {
-  for(int i = 0; i < 100; i++) {
-    cout << endl;
-  }
-};
-void ATMController:: begin() {
-  clearConsole();
-};
