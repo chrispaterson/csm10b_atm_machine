f5acaedde21268f3412b92e40ed78e7387cf7ad3
Chris Paterson <cpaterson@conversantmedia.com>
2017-12-10 20:06:41 -0800

    Adding Balance view view

--- AccountDetailView.cpp
+++ AccountDetailView.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "AccountDetailView.h"
 #include "NumberFormatter.h"
+#include "DisplayMesageView.h"
 #include <string>
 #include <iostream>
 
@@ -31,10 +32,29 @@ void AccountDetailView::show(Account *account) {
 		prompt = "CD";
 		break;
 	}
+
 	NumberFormatter numFormatter;
+
 	std::string balance = "$" + numFormatter.convert_to_two_decimal_string(account->balance);
 	prompt += " [" + std::to_string(account->id) + "] " + balance;
+
 	createMenu(prompt, actions, NUM_CHOICES);
+    switch(getSelected()) {
+      case 0:
+        // Deposit
+        break;
+      case 1:
+        // Withdrawal
+        break;
+      case 2:
+        // Balance
+        DisplayMesageView dmv;
+        dmv.displayMessage(balance + " Balance");
+        break;
+      case 3:
+        // Transfer
+        break;
+    }
 }
 
 AccountDetailView::~AccountDetailView()
